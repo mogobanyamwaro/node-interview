38bf4c8322f80b1edc4450e431ccd74a1ebbc08e
douglas mogoba <mogobanyamwaro@gmail.com>
2022-06-01 19:26:42 +0300

    bubble sort

--- example/hello.word.js
+++ example/hello.word.js
@@ -1,1 +1,29 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+console.log(bubbleSort([5, 3, 8]));
 
+//Insertion Sort
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
+}
