c4b019e965aefd753297783c8960699e4752d25d
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2022-07-22 21:12:57 +0300

    chore: steps algorithm done

--- algorithms/steps.shape.js
+++ algorithms/steps.shape.js
@@ -1,12 +1,13 @@
 function stepShape(n) {
   for (let i = 0; i <= n; i++) {
     let step = "";
-    for (let hash = n; hash > 0; hash--) {
+    for (let hash = i; hash > 0; hash--) {
       step += "#";
     }
-    for (let space = n - 1; space > 0; space--) {
-      step += "k  ";
+    for (let space = n - i; space > 0; space--) {
+      step += " ";
     }
+
     console.log(step);
   }
 }
